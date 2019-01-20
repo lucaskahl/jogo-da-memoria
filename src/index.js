a84e53937b04c7b61b36b1bba0f449990bdf3d5a
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-20 22:27:27 +0000

    Desafio aula 021

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $memoryCardSide = document.createElement("article");
 const $icon = `
   <img
     src="./img/icon-collabcode.svg"
@@ -8,7 +9,16 @@ const $icon = `
   />
 `;
 
+const $iconPhp = `
+      <div class="side">
+        <img src="./img/icon-php.png" alt="Gueio icon" class="icon" />
+      </div>
+`;
+
 $memoryCard.classList.add("memory-card");
+$memoryCardSide.classList.add("memory-card", "-active");
 
 $root.insertBefore($memoryCard, null);
+$root.insertBefore($memoryCardSide, null);
 $memoryCard.insertAdjacentHTML("afterbegin", $icon);
+$memoryCardSide.insertAdjacentHTML("afterbegin", $iconPhp);
