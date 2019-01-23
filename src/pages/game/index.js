ef428a15db5204176452ce59c0dbf4195f44cf47
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-23 23:24:08 +0000

    Desafio 1 - Aula 025

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -12,12 +12,32 @@ const $memoryCardFront = createMemoryCard(
   "Gueio mascote da CollabCode"
 );
 
+const $cardJava = createMemoryCard(
+  "img/icon-java.png",
+  "Ícone de um livro de Java",
+  "-front"
+);
+
+const $cardJavaScript = createMemoryCard(
+  "img/icon-js.png",
+  "Ícone de um livro de Javascript",
+  "-front"
+);
+
+const $cardC = createMemoryCard(
+  "img/icon-c.png",
+  "Ícone de um livro de C",
+  "-front"
+);
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardJavaScript);
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardJavaScript);
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $cardC);
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
