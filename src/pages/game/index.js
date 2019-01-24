7fb6074015b25f671205dfd87dc82a567e7b1cab
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-24 01:29:55 +0000

    Adicionado object destructuring aula 025

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,20 +1,22 @@
 const $root = document.querySelector("#root");
 
 const cards = {
-  default: ["img/icon-collabcode.svg", "Gueio mascote da CollabCode"],
-  c: ["img/icon-c.png", "Ícone de um livro de C", "-front"],
-  javascript: ["img/icon-js.png", "Ícone de um livro de Javascript", "-front"],
-  java: ["img/icon-java.png", "Ícone de um livro de Java", "-front"],
-  php: ["img/icon-php.png", "Ícone de um livro de Php", "-front"]
+  gueio: ["", "img/icon-collabcode.svg", "Gueio mascote da CollabCode"],
+  c: ["-front", "img/icon-c.png", "Ícone de um livro de C"],
+  javascript: ["-front", "img/icon-js.png", "Ícone de um livro de Javascript"],
+  java: ["-front", "img/icon-java.png", "Ícone de um livro de Java"],
+  php: ["-front", "img/icon-php.png", "Ícone de um livro de Php"]
 };
 
 const $cardsWrapper = createCardsWrapper();
 
-const $memoryCard = createMemoryCard(...cards.default);
-const $cardPhp = createMemoryCard(...cards.php);
-const $cardJava = createMemoryCard(...cards.java);
-const $cardJavaScript = createMemoryCard(...cards.javascript);
-const $cardC = createMemoryCard(...cards.c);
+const { gueio, c, javascript, java, php } = cards; // Desestruturação
+
+const $memoryCard = createMemoryCard(...gueio);
+const $cardPhp = createMemoryCard(...php);
+const $cardJava = createMemoryCard(...java);
+const $cardJavaScript = createMemoryCard(...javascript);
+const $cardC = createMemoryCard(...c);
 
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
