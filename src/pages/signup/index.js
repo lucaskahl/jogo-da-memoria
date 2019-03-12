94b1ca49c26e214a38946be0b8c90cddb6567636
lucaskahl <lucaschangkahl@gmail.com>
2019-03-11 23:26:49 -0300

    Desafio basico aula 038

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -2,6 +2,8 @@
   const $root = document.querySelector("#root");
 
   const $flatButton = flatButton.render();
+  const $flatButton2 = flatButton.render();
 
   $root.insertAdjacentHTML("beforeend", $flatButton);
+  $root.insertAdjacentHTML("beforeend", $flatButton2);
 })();
